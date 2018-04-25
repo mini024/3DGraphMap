20b1a9b0e9f5e9361f4249ba901af3513c184a0f
Jessica Cavazos <jessycavazos95@gmail.com>
2018-04-25 13:57:52 -0500

    Added world image

--- 3DGraphMap_Project/main.cpp
+++ 3DGraphMap_Project/main.cpp
@@ -1,15 +1,277 @@
 //
 //  main.cpp
-//  3DGraphMap_Project
+//  3DGrpahMap_Project
 //
-//  Created by Jessica M Cavazos Erhard on 4/25/18.
+//  Created by Jessica M Cavazos Erhard on 4/11/18.
 //  Copyright © 2018 Jessica M Cavazos Erhard. All rights reserved.
 //
 
-#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+// angle of rotation for the camera direction
+float angle = 0.0f;
+// actual vector representing the camera's direction
+float lx=0.0f,lz=-1.0f,ly=1.0f;
+// XZY position of the camera
+float x=0.0f, z=5.0f, y= 0.0f;
+// the key states. These variables will be zero
+//when no key is being presses
+float deltaAngle = 0.0f;
+float deltaMove = 0;
+float deltaMoveY = 0;
+
+void changeSize(int w, int h) {
+    
+    // Prevent a divide by zero, when window is too short
+    // (you cant make a window of zero width).
+    if (h == 0)
+        h = 1;
+    float ratio =  w * 1.0 / h;
+    
+    // Use the Projection Matrix
+    glMatrixMode(GL_PROJECTION);
+    
+    // Reset Matrix
+    glLoadIdentity();
+    
+    // Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+    
+    // Set the correct perspective.
+    gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+    
+    // Get Back to the Modelview
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void drawCube(float x, float y, float z)
+{
+    const float sizex = 0.5f;
+    const float sizey = 0.5f;
+    const float sizez = 0.5f;
+    
+    glTranslatef(-x, -y, -z);
+    
+    glBegin(GL_QUADS);
+    
+    glColor3f(1.0, 1.0, 0.0);
+    
+    // FRONT
+    glVertex3f(-sizex, -sizey, sizez);
+    glVertex3f(sizex, -sizey, sizez);
+    glVertex3f(sizex, sizey, sizez);
+    glVertex3f(-sizex, sizey, sizez);
+    
+    // BACK
+    glVertex3f(-sizex, -sizey, -sizez);
+    glVertex3f(-sizex, sizey, -sizez);
+    glVertex3f(sizex, sizey, -sizez);
+    glVertex3f(sizex, -sizey, -sizez);
+    
+    glColor3f(0.0, 1.0, 0.0);
+    
+    // LEFT
+    glVertex3f(-sizex, -sizey, sizez);
+    glVertex3f(-sizex, sizey, sizez);
+    glVertex3f(-sizex, sizey, -sizez);
+    glVertex3f(-sizex, -sizey, -sizez);
+    
+    // RIGHT
+    glVertex3f(sizex, -sizey, -sizez);
+    glVertex3f(sizex, sizey, -sizez);
+    glVertex3f(sizex, sizey, sizez);
+    glVertex3f(sizex, -sizey, sizez);
+    
+    glColor3f(0.0, 0.0, 1.0);
+    
+    // TOP
+    glVertex3f(-sizex, sizey, sizez);
+    glVertex3f(sizex, sizey, sizez);
+    glVertex3f(sizex, sizey, -sizez);
+    glVertex3f(-sizex, sizey, -sizez);
+    
+    // BOTTOM
+    glVertex3f(-sizex, -sizey, sizez);
+    glVertex3f(-sizex, -sizey, -sizez);
+    glVertex3f(sizex, -sizey, -sizez);
+    glVertex3f(sizex, -sizey, sizez);
+    
+    glEnd();
+    
+    glTranslatef(x, y, z);
+}
+
+void computePos(float deltaMove) {
+    
+    x += deltaMove * lx * 0.1f;
+    z += deltaMove * lz * 0.1f;
+}
+
+void computePosY(float deltaMove) {
+    
+    y += deltaMove * ly * 0.1f;
+    z += deltaMove * lz * 0.1f;
+}
+
+void computeDir(float deltaAngle) {
+    
+    angle += deltaAngle;
+    lx = sin(angle);
+    lz = -cos(angle);
+}
+
+GLuint LoadTexture( const char * filename )
+{
+    int width, height;
+    
+    unsigned char * data;
+    
+    FILE * file;
+    
+    file = fopen( filename, "rb" );
+    
+    if ( file == NULL ) return 0;
+    width = 1200;
+    height = 715;
+    data = (unsigned char *)malloc( width * height * 3 );
+    //int size = fseek(file,);
+    fread( data, width * height * 3, 1, file );
+    fclose( file );
+    
+    for(int i = 0; i < width * height ; ++i)
+    {
+        int index = i*3;
+        unsigned char B,R;
+        B = data[index];
+        R = data[index+2];
+        
+        data[index] = R;
+        data[index+2] = B;
+    }
+    
+    GLuint texture;
+    
+    glGenTextures( 1, &texture );
+    glBindTexture( GL_TEXTURE_2D, texture );
+    glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE,GL_MODULATE );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_NEAREST );
+    
+    
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_REPEAT );
+    gluBuild2DMipmaps( GL_TEXTURE_2D, 3, width, height,GL_RGB, GL_UNSIGNED_BYTE, data );
+    free( data );
+    
+    return texture;
+}
+
+void renderScene(void) {
+    
+    if (deltaMove)
+        computePos(deltaMove);
+    if (deltaMoveY)
+        computePosY(deltaMoveY);
+    if (deltaAngle)
+        computeDir(deltaAngle);
+    
+    // Clear Color and Depth Buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    // Reset transformations
+    glLoadIdentity();
+    // Set the camera
+    gluLookAt(    x, 1.0f, z,
+              x+lx, 10.0f,  z+lz,
+              0.0f, 1.0f,  0.0f);
+    glRotatef(90, 1.0, 0.0, 0.0);
+    
+    // Image
+    GLuint texture;
+    // Agregar path completo de imagen.
+    texture = LoadTexture("~/3DGraphMap_Project/3DGraphMap_Project/world-map.bmp");
+    glBindTexture (GL_TEXTURE_2D, texture);
+    glEnable(GL_TEXTURE_2D);
+    
+    // Draw ground
+    
+    glColor3f(0.9f, 0.9f, 0.9f);
+    glBegin(GL_QUADS);
+    glVertex3f(-100.0f, 0.0f, -100.0f);
+    glVertex3f(-100.0f, 0.0f,  100.0f);
+    glVertex3f( 100.0f, 0.0f,  100.0f);
+    glVertex3f( 100.0f, 0.0f, -100.0f);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+    
+    // Draw Cubes
+    
+    for(int i = -3; i < 3; i++)
+        for(int j=-3; j < 3; j++) {
+            glPushMatrix();
+            glTranslatef(i*10.0,0,j * 10.0);
+            drawCube(0.0,0.0,0.0);
+            glPopMatrix();
+        }
+    
+    glutSwapBuffers();
+}
+
+
+void pressKey(int key, int xx, int yy) {
+    
+    switch (key) {
+        case GLUT_KEY_LEFT : deltaAngle = -0.01f; break;
+        case GLUT_KEY_RIGHT : deltaAngle = 0.01f; break;
+        case GLUT_KEY_UP : deltaMove = 0.5f; break;
+        case GLUT_KEY_DOWN : deltaMove = -0.5f; break;
+        case 'a': deltaMoveY = -0.5f; break;
+    }
+}
+
+void releaseKey(int key, int x, int y) {
+    
+    switch (key) {
+        case GLUT_KEY_LEFT :
+        case GLUT_KEY_RIGHT : deltaAngle = 0.0f;break;
+        case GLUT_KEY_UP :
+        case GLUT_KEY_DOWN : deltaMove = 0;break;
+        case 'a':deltaMoveY = 0;break;
+    }
+}
+
+int main(int argc, char **argv) {
+    // init GLUT and create window
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(320,320);
+    glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+    
+    // register callbacks
+    glutDisplayFunc(renderScene);
+    glutReshapeFunc(changeSize);
+    glutIdleFunc(renderScene);
+    
+    glutSpecialFunc(pressKey);
+    
+    // here are the new entries
+    glutIgnoreKeyRepeat(1);
+    glutSpecialUpFunc(releaseKey);
+    
+    // OpenGL init
+    glEnable(GL_DEPTH_TEST);
+    
+    // enter GLUT event processing cycle
+    glutMainLoop();
+    
+    return 1;
 }
