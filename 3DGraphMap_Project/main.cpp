11eaf9fe19f0a247465a7f01951cf988a7a3cadd
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2018-05-08 22:41:58 -0500

    Fixed bars rendering with sphere coordinates

--- 3DGraphMap_Project/main.cpp
+++ 3DGraphMap_Project/main.cpp
@@ -3,18 +3,46 @@
 //  3DGrpahMap_Project
 //
 //  Created by Jessica M Cavazos Erhard on 4/11/18.
-//  Copyright © 2018 Jessica M Cavazos Erhard. All rights reserved.
+//  Copyright © 2018
+//  Jessica M Cavazos Erhard.
+//  José Antonio Ruiz del Moral Cervantes
+//  All rights reserved.
+//
 //
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
-
-#ifdef __APPLE__
+#include <stdlib.h>
+#include <OpenGL/gl.h>
 #include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
+#include <math.h>
+#include <sys/time.h>
+#include <vector>
+#include "Planet.h"
+
+//#include "imgui.h"
+//#include "imgui_impl_glut.h"
+
+using namespace std;
+
+//void DoGUI();
+
+// GLobal Variables
+float diffuseColor[] = {1.0f, 0.0f, 0.0f, 1.0f};
+float lightDirection[] = {2.0f, 2.0f, 2.0f, 1.0f};
+float eyePos[] = {-11.0f, 8.0f, -7.0f};
+float target[] = {0.0f, 0.0f, 0.0f};
+bool rotateModel = false;
+float attenC = 1.0f;
+float attenL = 0.0f;
+float attenQ = 0.0f;
+
+bool planeVisible = true;
+float planeScale[] = {10.0f, 1.0f, 10.0f};
+float planeTranslate[] = {0.0f, 0.0f, 0.0f};
+
+bool sphereVisible = true;
+float sphereScale[] = {1.0f, 1.0f, 1.0f};
+float sphereTranslate[] = {0.0f, 3.0f, 0.0f};
 
 // angle of rotation for the camera direction
 float angle = 0.0f;
@@ -27,6 +55,9 @@ float x=0.0f, z=5.0f, y= 0.0f;
 float deltaAngle = 0.0f;
 float deltaMove = 0;
 float deltaMoveY = 0;
+
+// variable to store planet (global)
+Planet earth;
 
 void changeSize(int w, int h) {
     
@@ -54,9 +85,9 @@ void changeSize(int w, int h) {
 
 void drawCube(float x, float y, float z)
 {
-    const float sizex = 0.5f;
-    const float sizey = 0.5f;
-    const float sizez = 0.5f;
+    const float sizex = 0.2f;
+    const float sizey = 0.2f;
+    const float sizez = 15.5f;
     
     glTranslatef(-x, -y, -z);
     
@@ -109,6 +140,42 @@ void drawCube(float x, float y, float z)
     glTranslatef(x, y, z);
 }
 
+void getPointFromLatandLong(float latitude,float longitude ,char city){
+    
+    float X,Y,Z;
+    latitude = latitude * M_PI / 180;
+    longitude = longitude * M_PI / 180;
+    
+    int f  = 0;// flattening
+    int alt = 1;
+    float ls = atan((1 - f)*2 * tan(latitude));// lambda
+    
+    X = 10.0 * cos(ls) * cos(longitude) + alt * cos(latitude) * cos(longitude);
+    Y = 10.0 * cos(ls) * sin(longitude) + alt *  cos(latitude) * sin(longitude);
+    Z = 10.0 * sin(ls) + alt * sin(latitude);
+    
+    switch (city) {
+        case '1':
+            drawCube(X, Y-13.2, Z); //dubai
+            break;
+        case '2':
+            drawCube(X, Y-12.2, Z); //moscow
+            break;
+        case '3':
+            drawCube(X-1.3, Y-13.2, Z); //seoul
+            break;
+        case '4':
+            drawCube(X+1.1, Y-14.6, Z); //singapore
+            break;
+
+        default:
+            break;
+    }
+    
+    //drawCube(X+1.1, Y-14.6, Z);
+    
+}
+
 void computePos(float deltaMove) {
     
     x += deltaMove * lx * 0.1f;
@@ -116,63 +183,57 @@ void computePos(float deltaMove) {
 }
 
 void computePosY(float deltaMove) {
-    
     y += deltaMove * ly * 0.1f;
     z += deltaMove * lz * 0.1f;
 }
 
 void computeDir(float deltaAngle) {
-    
     angle += deltaAngle;
     lx = sin(angle);
     lz = -cos(angle);
 }
 
-GLuint LoadTexture( const char * filename )
-{
-    int width, height;
-    
-    unsigned char * data;
-    
-    FILE * file;
-    
-    file = fopen( filename, "rb" );
-    
-    if ( file == NULL ) return 0;
-    width = 1200;
-    height = 715;
-    data = (unsigned char *)malloc( width * height * 3 );
-    //int size = fseek(file,);
-    fread( data, width * height * 3, 1, file );
-    fclose( file );
-    
-    for(int i = 0; i < width * height ; ++i)
-    {
-        int index = i*3;
-        unsigned char B,R;
-        B = data[index];
-        R = data[index+2];
-        
-        data[index] = R;
-        data[index+2] = B;
-    }
-    
-    GLuint texture;
-    
-    glGenTextures( 1, &texture );
-    glBindTexture( GL_TEXTURE_2D, texture );
-    glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE,GL_MODULATE );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_NEAREST );
-    
-    
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT );
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_REPEAT );
-    gluBuild2DMipmaps( GL_TEXTURE_2D, 3, width, height,GL_RGB, GL_UNSIGNED_BYTE, data );
-    free( data );
-    
-    return texture;
-}
+//void DoGUI(){
+//    ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoResize |
+//    ImGuiWindowFlags_NoCollapse |
+//    ImGuiWindowFlags_NoMove;
+//    glDisable(GL_LIGHTING);
+//    ImGui_ImplGLUT_NewFrame((int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y, 1.0f / 60.0f);
+//
+//    static bool showLightConfig = true;
+//    static bool showAppConfig = true;
+//    static bool firstRun = true;
+//
+//    if (firstRun) {
+//        ImGui::SetNextWindowPos(ImVec2(10, 10));
+//        ImGui::SetNextWindowSize(ImVec2(330,120));
+//    }
+//    ImGui::Begin("Camera", &showLightConfig, window_flags);
+//    ImGui::DragFloat3("Eye Position", eyePos, 0.25f);
+//    ImGui::DragFloat3("Target", target, 0.25f);
+//    ImGui::Checkbox("Rotate View", &rotateModel);
+//    ImGui::End();
+//
+//    if (firstRun) {
+//        ImGui::SetNextWindowPos(ImVec2(670,10));
+//        ImGui::SetNextWindowSize(ImVec2(120,80));
+//    }
+//    ImGui::Begin("Application", &showAppConfig, window_flags);
+//
+//    if (ImGui::Button("Quit")) {
+//        exit(0);
+//    }
+//    if (ImGui::Button("Reset")) {
+//        exit(0);
+//    }
+//    ImGui::End();
+//
+//
+//    glViewport(0, 0, (int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
+//    ImGui::Render();
+//    glEnable(GL_LIGHTING);
+//    firstRun = false;
+//}
 
 void renderScene(void) {
     
@@ -181,6 +242,7 @@ void renderScene(void) {
     if (deltaMoveY)
         computePosY(deltaMoveY);
     if (deltaAngle)
+        computePos(deltaMove);
         computeDir(deltaAngle);
     
     // Clear Color and Depth Buffers
@@ -189,39 +251,33 @@ void renderScene(void) {
     // Reset transformations
     glLoadIdentity();
     // Set the camera
-    gluLookAt(    x, 1.0f, z,
+    gluLookAt(x, 1.0f, z,
               x+lx, 10.0f,  z+lz,
               0.0f, 1.0f,  0.0f);
     glRotatef(90, 1.0, 0.0, 0.0);
     
-    // Image
-    GLuint texture;
-    // Agregar path completo de imagen.
-    texture = LoadTexture("~/3DGraphMap_Project/3DGraphMap_Project/world-map.bmp");
-    glBindTexture (GL_TEXTURE_2D, texture);
-    glEnable(GL_TEXTURE_2D);
+    // position update
+    earth.x0=  0.0;
+    earth.y0=  0.0;
+    earth.z0=  -30.0;
     
-    // Draw ground
+    // add this to render loop
+    earth.draw(); // draws the planet
+    //earth.t+=lz + 1; // just rotate planet by 2.5 deg each frame...
     
-    glColor3f(0.9f, 0.9f, 0.9f);
-    glBegin(GL_QUADS);
-    glVertex3f(-100.0f, 0.0f, -100.0f);
-    glVertex3f(-100.0f, 0.0f,  100.0f);
-    glVertex3f( 100.0f, 0.0f,  100.0f);
-    glVertex3f( 100.0f, 0.0f, -100.0f);
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
+    getPointFromLatandLong(37.566667,126.966667,'3'); //Seoul
+    getPointFromLatandLong(55.75,37.616667,'2'); //Moscow
+    getPointFromLatandLong(25.263056,55.297222,'1'); //Dubai
+    getPointFromLatandLong(1.283333,103.833333,'4'); //Singapore
     
-    // Draw Cubes
+     //   getPointFromLatandLong(10.480556,-66.903611); //Caracas
+    //    getPointFromLatandLong(-15.793889,-47.882778); //Brasilia
+    //    getPointFromLatandLong(38.833333,-104.816667); //Colorado spgs
+    //    getPointFromLatandLong(19.433333,-99.133333); //CDMX (jala como colorado)
+
     
-    for(int i = -3; i < 3; i++)
-        for(int j=-3; j < 3; j++) {
-            glPushMatrix();
-            glTranslatef(i*10.0,0,j * 10.0);
-            drawCube(0.0,0.0,0.0);
-            glPopMatrix();
-        }
     
+    // DoGUI();
     glutSwapBuffers();
 }
 
@@ -248,13 +304,18 @@ void releaseKey(int key, int x, int y) {
     }
 }
 
+//Global Variables
+static int glutWindowId = 0;
+
 int main(int argc, char **argv) {
     // init GLUT and create window
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowPosition(100,100);
-    glutInitWindowSize(320,320);
+    glutInitWindowSize(800,600);
     glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+    
+    glutWindowId = glutGetWindow();
     
     // register callbacks
     glutDisplayFunc(renderScene);
@@ -270,8 +331,13 @@ int main(int argc, char **argv) {
     // OpenGL init
     glEnable(GL_DEPTH_TEST);
     
+    // init after OpenGL initialisation
+    earth.init(10.0,"/Users/alfonso/Google Drive/ITC 8vo Semestre/Graficas Computacionales/3DGraphMap/3DGraphMap_Project/world_map.bmp");
+    
     // enter GLUT event processing cycle
     glutMainLoop();
     
     return 1;
 }
+
+
